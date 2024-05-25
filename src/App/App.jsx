81e33a74da9935d65388e038afdba8f55a444ba1
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-05-25 23:50:20 +0200

    fix

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,21 @@
-import { useState, useEffect } from 'react'
- 
-import './App.css'
-import ContactList
+import { useEffect, useState } from "react";
 
+import "./App.css";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactForm from "./components/ContactForm/ContactForm";
 
-export default App
+import contactsData from "./contacts.json";
+
+const LS_CONTACTS_KEY = "initial-contacts";
+
+const initialContacts = () => {
+  const localStorageContacts = localStorage.getItem(LS_CONTACTS_KEY);
+  return localStorageContacts ? JSON.parse(localStorageContacts) : contactsData;
+};
+
+function App() {
+  const [contacts, setContacts] = useState(initialContacts);
+  const [filter, setFilter] = useState("");
+}
+export default App;
